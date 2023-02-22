095a6acd12095e9175237f4bdc0fe9bcd5c8678e
jwywoo <jwywoo26@gmail.com>
2023-02-21 22:32:20 -0500

    Bubble Sort Done

--- bubbleSort/src/Main.java
+++ bubbleSort/src/Main.java
@@ -13,7 +13,26 @@ public class Main {
         for (int i = 0; i < N; i++) {
             System.out.print(A[i] + " ");
         }
-        System.out.println("");
+        System.out.println(" ");
+        // Bubble sort
+        int sorted = A.length - 1;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < sorted; j++ ){
+                // curr and next
+                int curNum = A[j];
+                int nextNum = A[j + 1];
+                // compare
+                if (curNum > nextNum) {
+                    A[j] = nextNum;
+                    A[j+1] = curNum;
+                }
+            }
+        }
+        // Checking the answer
+        for (int i = 0; i < N; i++) {
+            System.out.print(A[i] + " ");
+        }
+        System.out.println(" ");
 
     }
 }
